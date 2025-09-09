36a69e13a13123fa0a13996497149ddc3e451fb2
EGY777 <egys5802@gmail.com>
2025-09-09 11:38:43 +0700

    Days002.java

---
+++ Days002.java
@@ -0,0 +1,11 @@
+package days002;
+
+public class Days002 {
+
+        public static void main(String[] args) {
+            System.out.println("Hello world");
+            System.out.print("Hello");
+            System.out.printf(" world");
+    }
+    
+}
