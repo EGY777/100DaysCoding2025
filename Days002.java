dc49ec1046a9e37a727fb94a0f31f1e4ef9dd0f8
EGY777 <egys5802@gmail.com>
2025-09-09 11:37:38 +0700

    Delete Days002.java

--- Days002.java
+++
@@ -1,8 +0,0 @@
-package days002;
-
-public class Days002 {
-
-        public static void main(String[] args) {
-            System.out.println("Hello world");
-            System.out.print("Hello");
-            System.out.printf(" world");
