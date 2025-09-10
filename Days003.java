ba2f40c7c33120089dcd74f8c94c75284d2015c6
EGY777 <egys5802@gmail.com>
2025-09-10 09:12:08 +0700

    Days003.java

---
+++ Days003.java
@@ -0,0 +1,15 @@
+package days003;
+
+public class Days003 {
+  
+    public static void main(String[] args) {
+        // Komentar satu baris
+        
+        /*
+        komentar multibaris
+        atau
+        lebih dari satu baris 
+        */
+    }
+    
+}
