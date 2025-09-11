2efdd5255f1b6cee2ca49cff3df1710eed873348
EGY777 <egys5802@gmail.com>
2025-09-11 15:05:05 +0700

    Days004.java

---
+++ Days004.java
@@ -0,0 +1,18 @@
+
+package days004;
+
+public class Days004 {
+
+    public static void main(String[] args) {
+        // kode (\t) menambah spasi
+        System.out.println("nama :\tEgy");
+        //kode (\\) menambah garis miring terbalik pada ouputan
+        System.out.println("contoh : \\");
+        // kode (\n) memindahkan teks ke garis baru
+        System.out.println("contohnya seperti ini \n hay");
+        //kode (\r) menghilangkan teks sebelumnya
+        System.out.println("contoh \rhay");
+        
+    }
+    
+}
