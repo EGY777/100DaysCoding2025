98590f5cbb9d2fa7a4f616fffc0c793090bcf4c7
EGY777 <egys5802@gmail.com>
2025-09-13 18:36:21 +0700

    days006.java

---
+++ days006.java
@@ -0,0 +1,22 @@
+package days006;
+
+public class Days006 {
+
+    
+    public static void main(String[] args) {
+       // Tipe Data Bilangan Pecahan(desimal) ada dua yaitu
+       
+       // yang pertama itu ada tipe data floating pint (float)
+       
+      float harga = (float) 4.8;
+        System.out.println("harga sepatu adidas adizero = "+ harga +"jt.");
+        
+        // yang kedua itu ada tipe data double floating pint (double)
+        
+        double panjang = 12.3;
+        System.out.println("jarak rata rata joging sejauh = "+ panjang +"KM.");
+       
+       
+    }
+    
+}
