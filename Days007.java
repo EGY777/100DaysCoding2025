9d0fe203959ebe7c61981e75dcbf8661cc3cc180
EGY777 <egys5802@gmail.com>
2025-09-14 13:14:53 +0700

    Days007.java

---
+++ Days007.java
@@ -0,0 +1,13 @@
+ //Tipe data karakter (char) dan tpe data boolean (boolean)
+        
+       // Tipe data karakter(char)yang dimama tipe datanya itu menggunakan tanda petik satu('')untuk menampilkan satu karater tunggal.
+       //contoh
+       
+       char Gender = 'L';
+        System.out.println("jenis kelamin = " + Gender );
+        
+        // Tipe data boolean(boolean)yang dimana tipe datanya itu cuman menampilkan true/false.
+        //contoh
+        
+        boolean sehat = true;
+        System.out.println("apakah badan saya sehat = "+ sehat );
