4f49b1acc44f7872c46e0c67dc22f6d380ff414b
EGY777 <egys5802@gmail.com>
2025-09-15 18:51:24 +0700

    Day008.java

---
+++ Day008.java
@@ -0,0 +1,13 @@
+package day008;
+
+
+public class Day008 {
+    public static void main(String[] args) {
+        String nama = "muh. Egy saputra bali";
+        String hoby = "karate dan joging";
+        System.out.println(" nama saya = " + nama);
+        System.out.println(" hoby saya = " + hoby);
+        
+    }
+    
+}
