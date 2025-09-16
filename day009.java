198db888c4d0520d53f97b66b424da48984b9dc5
EGY777 <egys5802@gmail.com>
2025-09-16 12:35:36 +0700

    day009.java

---
+++ day009.java
@@ -0,0 +1,31 @@
+
+package pkg009;
+
+public class Main {
+    public static void main(String[] args) {
+        //Deklarasi Variabel
+        
+        //inisialisasi Variabel :
+        int umur = 16;
+        double tinggi = 169.3;
+        float beratbadan = 47.5f;
+        
+        System.out.println("TAHUN 2023");
+        System.out.println("umur = "+ umur );
+        System.out.println("tinggi badan = "+ tinggi);
+        System.out.println("berat badan = "+ beratbadan);
+        
+        
+        //Updet Variabel :
+        
+         umur = 19; 
+         tinggi = 178.9;
+         beratbadan = 66.4f;
+         System.out.println("\nTAHUN 2025");
+         System.out.println("umur = "+ umur );
+         System.out.println("tinggi badan = "+ tinggi );
+         System.out.println("berat badan = "+ beratbadan );
+         
+    }
+    
+}
