af43d9a2d395c936384f49345752b5ef1dfda2cb
EGY777 <egys5802@gmail.com>
2025-09-17 21:18:28 +0700

    day10.java

---
+++ day10.java
@@ -0,0 +1,20 @@
+
+package pkg010;
+public class Main {
+    public static void main(String[] args) {
+        /*
+        final variabel yang dimana nilainy itu adalah nilai yang 
+            tetap atau nilai yang tidak bisah di ubah
+        dan jika di ubah dia akan eror
+        */
+              final String nama = "asep recing";
+           
+                 System.out.println("nama \t= "+ nama);
+           
+           String namaML = "Asep_Tzy";
+           
+           System.out.println("nama ML = "+ namaML);
+           
+    }
+    
+}
