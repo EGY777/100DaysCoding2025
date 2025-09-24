ec904c9bc2ffa2b1b0db77056eb509e9043920c1
EGY777 <egys5802@gmail.com>
2025-09-24 20:05:29 +0700

    day017.java

---
+++ day017.java
@@ -0,0 +1,12 @@
+package pkg017;
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        
+        int hasil = a%b;
+        
+        System.out.println( " 10%3 = "+hasil );
+    }
+    
+}
