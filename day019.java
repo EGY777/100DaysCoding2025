12a04020e44e205bb9c91706415758a4f5cc4286
EGY777 <egys5802@gmail.com>
2025-09-26 21:38:23 +0700

    day019.java

---
+++ day019.java
@@ -0,0 +1,16 @@
+package day019.java;
+public class Day019Java {
+    public static void main(String[] args) {
+        double angkadouble = 123.45;
+        
+        int angkaint = (int) angkadouble;
+        short angkashort = (short) angkaint;
+        byte angkabyte = (byte) angkaint;
+        
+        System.out.println(" nilai double : "+angkadouble);
+        System.out.println(" nilai int : "+angkaint);
+        System.out.println(" nilai short : "+angkashort);
+        System.out.println(" nilai byte : "+angkabyte);
+    }
+    
+}
