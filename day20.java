4ae48c592879101d1c8863d825198c982b45b865
EGY777 <egys5802@gmail.com>
2025-09-27 21:12:08 +0700

    day20.java

---
+++ day20.java
@@ -0,0 +1,20 @@
+package day20;
+public class Day20 {
+    public static void main(String[] args) {
+        //metode tostring
+        int angkaInt = 54321;
+        String konversi = Integer.toString(angkaInt);
+        
+        //metode velueof
+        short nilai =555; 
+        String teks = String.valueOf(nilai);
+        
+        //metode operator 
+        long angka = 777;
+        String ukuran = angka + "";
+        
+        System.out.println("int ke String: " +konversi);
+        System.out.println("short ke string: "+teks);
+        System.out.println("long ke String: "+ukuran);
+    }
+}
