6118bcfc4266f17d94a4fd33b944d6cf2fa4b30a
EGY777 <egys5802@gmail.com>
2025-10-06 21:27:51 +0800

    day029.java

---
+++ day029.java
@@ -0,0 +1,12 @@
+public class Perbandingan {
+    public static void main(String[] args) {
+      
+        int x = 50;
+        int y = 60;
+      
+        //MENGGUNAKAN OPERATOR <
+        System.out.println("x < y : " + (x < y));  // true
+        //MENGGUNAKAN OPERATOR >
+        System.out.println("x > y : " + (x > y));  // false
+    }
+}
