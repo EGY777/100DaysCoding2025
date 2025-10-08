468d0bdd98ab28cbba58b0eb0fb12c045220ea43
EGY777 <egys5802@gmail.com>
2025-10-08 21:29:27 +0700

    day031.java

---
+++ day031.java
@@ -0,0 +1,20 @@
+package day031;
+import java.util.Scanner;
+public class Day031 {
+    public static void main(String[] args) {
+             Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan umur Anda: ");
+        int umur = input.nextInt();
+
+        System.out.print("Masukkan nilai tes Anda: ");
+        int nilaiTes = input.nextInt();
+
+        if (umur >= 18 && nilaiTes >= 80) {
+            System.out.println("Anda lolos pendaftaran.");
+        } else {
+            System.out.println("Anda tidak memenuhi syarat.");
+        }
+    }
+    
+}
