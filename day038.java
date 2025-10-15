43ec5c7004a0dc02c0720d43786c5d831c1de45e
EGY777 <egys5802@gmail.com>
2025-10-15 19:17:32 +0700

    day038.java

---
+++ day038.java
@@ -0,0 +1,19 @@
+package javaapplication85;
+import java.util.Scanner;
+public class JavaApplication85 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner (System.in);
+        System.out.print("Masukan angka = ");
+        int a;
+        a = u.nextInt();
+        
+        if (a > 0){
+            System.out.println(a+" = positif");
+        } else if(a < 0){
+            System.out.println(a+" = negatif");   
+        }else{
+            System.out.println("NOL");
+        }
+    }
+    
+}
