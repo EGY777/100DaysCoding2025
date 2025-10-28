286b0f93f7213d7162916e03e875b4f508546662
EGY777 <egys5802@gmail.com>
2025-10-28 20:47:50 +0700

    day051.java

---
+++ day051.java
@@ -0,0 +1,16 @@
+package day051;
+import java.util.Scanner;
+public class Day051 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+        System.out.print("Masukan angka = ");
+         int angka = u.nextInt();
+        for (int i = 1; i < angka; i++) {
+            System.out.println("Peulangan ke-"+i);
+        }
+            
+            
+        }
+        
+    }
+    
