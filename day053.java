febe69580fddc306b0e44ec2bba8a09cd2216d7d
EGY777 <egys5802@gmail.com>
2025-10-30 22:19:08 +0800

    day053.java

---
+++ day053.java
@@ -0,0 +1,11 @@
+package kingMU;
+public class KingMU{
+   public static void main(String[] args) {
+   for (int angka = 1; angka <= 10; angka++) {
+    if (angka == 5) {
+        break;
+    }
+    System.out.println("angka = " + angka);
+}
+   }
+}
