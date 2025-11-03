ea399dd97757b2f803c0ed28dcc39ecdb3e40dcc
EGY777 <egys5802@gmail.com>
2025-11-03 20:45:14 +0700

    day057.java

---
+++ day057.java
@@ -0,0 +1,12 @@
+package pkg20;
+public class NewClass2 {
+    public static void main(String[] args) {
+        int angka = 10;
+        for (int i = 0; i < angka; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.println( j+", ASEP RACING"); 
+            }
+            System.out.println();
+        }
+    }
+}
