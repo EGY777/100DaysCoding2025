2a810053d682f685ee955145334b8a9a10bd5937
EGY777 <egys5802@gmail.com>
2025-11-04 22:14:56 +0700

    day058.java

---
+++ day058.java
@@ -0,0 +1,18 @@
+
+package pkg58;
+public class Main {
+    public static void main(String[] args) {
+       int n =7;
+       
+        for (int i = 0; i < n; i++) {
+            System.out.println(i+"");
+            
+        }
+        System.out.println(" ");
+        for (int i = n; i < 0; i++) {
+            System.out.println(i+"");
+        }
+        System.out.println("");
+    }
+    
+}
