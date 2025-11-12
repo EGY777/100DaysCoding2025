4f35b3fa570e3d1c4daadc3c5eb03b58f5a6c71c
EGY777 <egys5802@gmail.com>
2025-11-12 20:17:18 +0700

    day066.java

---
+++ day066.java
@@ -0,0 +1,14 @@
+package king;
+import java.util.Scanner;
+public class King {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+        System.out.print("Masukan angka = ");
+        int angka = u.nextInt();
+        for (int i = 0; i < angka; i++) {
+            System.out.println("*");
+            
+        }
+    }
+    
+}
