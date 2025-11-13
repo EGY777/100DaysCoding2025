1b64906f2fae1b2c524fbb364459ccecabdf0829
EGY777 <egys5802@gmail.com>
2025-11-13 20:51:26 +0800

    day067.java

---
+++ day067.java
@@ -0,0 +1,15 @@
+package king;
+import java.util.Scanner;
+public class King {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+        System.out.print("Masukan kolom horizontal = ");
+        int k = u.nextInt();
+        for (int i = 0; i < k; i++) {
+            System.out.println("*");
+            
+        }
+      System.out.println();
+    }
+    
+}
