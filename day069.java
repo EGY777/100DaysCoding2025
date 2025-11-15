235357aa9ae26af2276289cf3040f933bcc725af
EGY777 <egys5802@gmail.com>
2025-11-15 20:49:06 +0700

    day069.java

---
+++ day069.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+public class Day069 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+         String Username = "admin";
+         String Pasword = "12345";
+         int Mxpercobaan = 3;
+         int percobaan = 0;
+         
+         while (percobaan < Mxpercobaan){
+             System.out.print("Masukan username = ");
+             String username = u.nextLine();
+             System.out.print("Masukan Pasword = ");
+             String pasword = u.nextLine();
+             
+             if (username.equals(Username) && pasword.equals(Pasword)) {
+                 System.out.println("Login Berhasil Selamat datang");
+                 return;
+                 
+             }else{
+                 System.out.println("Login gagal coba lagi");
+                 percobaan++;
+             }
+             System.out.println("akses ditolak");
+         }
+    }
+    
+}
+import java.util.Scanner;
+public class Day069 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+         String Username = "admin";
+         String Pasword = "12345";
+         int Mxpercobaan = 3;
+         int percobaan = 0;
+         
+         while (percobaan < Mxpercobaan){
+             System.out.print("Masukan username = ");
+             String username = u.nextLine();
+             System.out.print("Masukan Pasword = ");
+             String pasword = u.nextLine();
+             
+             if (username.equals(Username) && pasword.equals(Pasword)) {
+                 System.out.println("Login Berhasil Selamat datang");
+                 return;
+                 
+             }else{
+                 System.out.println("Login gagal coba lagi");
+                 percobaan++;
+             }
+             System.out.println("akses ditolak");
+         }
+    }
+    
+}
+import java.util.Scanner;
+public class Day069 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+         String Username = "admin";
+         String Pasword = "12345";
+         int Mxpercobaan = 3;
+         int percobaan = 0;
+         
+         while (percobaan < Mxpercobaan){
+             System.out.print("Masukan username = ");
+             String username = u.nextLine();
+             System.out.print("Masukan Pasword = ");
+             String pasword = u.nextLine();
+             
+             if (username.equals(Username) && pasword.equals(Pasword)) {
+                 System.out.println("Login Berhasil Selamat datang");
+                 return;
+                 
+             }else{
+                 System.out.println("Login gagal coba lagi");
+                 percobaan++;
+             }
+             System.out.println("akses ditolak");
+         }
+    }
+    
+}
+import java.util.Scanner;
+public class Day069 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+         String Username = "admin";
+         String Pasword = "12345";
+         int Mxpercobaan = 3;
+         int percobaan = 0;
+         
+         while (percobaan < Mxpercobaan){
+             System.out.print("Masukan username = ");
+             String username = u.nextLine();
+             System.out.print("Masukan Pasword = ");
+             String pasword = u.nextLine();
+             
+             if (username.equals(Username) && pasword.equals(Pasword)) {
+                 System.out.println("Login Berhasil Selamat datang");
+                 return;
+                 
+             }else{
+                 System.out.println("Login gagal coba lagi");
+                 percobaan++;
+             }
+             System.out.println("akses ditolak");
+         }
+    }
+    
+}
+import java.util.Scanner;
+public class Day069 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+         String Username = "admin";
+         String Pasword = "12345";
+         int Mxpercobaan = 3;
+         int percobaan = 0;
+         
+         while (percobaan < Mxpercobaan){
+             System.out.print("Masukan username = ");
+             String username = u.nextLine();
+             System.out.print("Masukan Pasword = ");
+             String pasword = u.nextLine();
+             
+             if (username.equals(Username) && pasword.equals(Pasword)) {
+                 System.out.println("Login Berhasil Selamat datang");
+                 return;
+                 
+             }else{
+                 System.out.println("Login gagal coba lagi");
+                 percobaan++;
+             }
+             System.out.println("akses ditolak");
+         }
+    }
+    
+}
