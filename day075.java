8d9865b8865d2c1a83bfcc93fea44984a295f9c2
EGY777 <egys5802@gmail.com>
2025-11-21 22:07:13 +0800

    day075.java

---
+++ day075.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Day75 {
+    public static void main(String[] args) {
+        Scanner u = new Scanner (System.in);
+        System.out.print("Masukan Nama : ");
+        String n = in.nextLine();
+        
+        System.out.println("Panjang Nama \t: " + n.length());
+        System.out.println("Nama Besar\t: " + n.toUpperCase());
+        System.out.println("Nama kecil\t: " + n.toLowerCase());
+
+    }
+}
