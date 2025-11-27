0deeb8eeb4ea55848b56e2601a040e546876bef8
EGY777 <egys5802@gmail.com>
2025-11-27 19:11:27 +0700

    day081.java

---
+++ day081.java
@@ -0,0 +1,16 @@
+
+package array;
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+        
+        System.out.print("Masukan indeks = ");
+        int[] a = {18,7,20,40,50};
+       
+        int b = u.nextInt();
+         System.out.println("NIlai indeks "+b+" = "+a[b]);
+         
+    }
+    
+}
