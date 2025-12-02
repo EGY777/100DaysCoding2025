2f768934ccc3dbc286780f2091a663ae83140826
EGY777 <egys5802@gmail.com>
2025-12-02 21:28:20 +0800

    day086.java

---
+++ day086.java
@@ -0,0 +1,13 @@
+package king;
+public class King {
+    public static void main(String[] args) {
+        int [] angka = {10,30,20,1,80,50};
+        int minimum = angka[0];
+        for (int i = 0; i < angka.length; i++) {
+            if (angka[i] < minimum) {
+                minimum = angka[i];
+            }
+        }
+        System.out.println("Nilai minimum : " + minimum);
+    }
+}
