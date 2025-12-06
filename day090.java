628291e53ff7eb61c8e7fc2b1b16e755363d4a25
EGY777 <egys5802@gmail.com>
2025-12-06 21:44:50 +0800

    day090.java
    Changed method signature to public and modified return type.

---
+++ day090.java
@@ -0,0 +1,11 @@
+public class MethodNonVoid {
+
+    public static void int nilaiAkhir() {
+        return 25; 
+    }
+
+    public static void main(String[] args) {
+        int n = nilaiAkhir();
+        System.out.println("Nilai: " + n);
+    }
+}
