e2e61179ae83bd19bbb6af7ab9b08f871fcead79
EGY777 <egys5802@gmail.com>
2025-12-08 22:14:46 +0800

    day092.java

---
+++ day092.java
@@ -0,0 +1,12 @@
+package ya;
+public class Ya {
+    public static int tambah(int a, int b) {
+        int hasil = a + b;
+        return hasil;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(10, 15);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
+   
