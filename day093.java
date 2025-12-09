3a438603b10af11a46cedf632d068af15942be8c
EGY777 <egys5802@gmail.com>
2025-12-09 22:05:40 +0800

    day093.java

---
+++ day093.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double angka = 4.7;
+        System.out.println("ceil: " + Math.ceil(x));
+        System.out.println("floor: " + Math.floor(x));
+        System.out.println("round: " + Math.round(x));
+    }
+}
